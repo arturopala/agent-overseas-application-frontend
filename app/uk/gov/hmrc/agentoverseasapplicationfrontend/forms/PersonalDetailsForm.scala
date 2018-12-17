d53914d25693acbc18b691f8d774d357d78291d0
rvijayan <rvijayan@users.noreply.github.com>
2018-12-17 10:47:25 +0000

    [ASA-364][RV] add validations for personal-details form (#24)

--- app/uk/gov/hmrc/agentoverseasapplicationfrontend/forms/PersonalDetailsForm.scala
+++ app/uk/gov/hmrc/agentoverseasapplicationfrontend/forms/PersonalDetailsForm.scala
@@ -3,13 +3,14 @@ package uk.gov.hmrc.agentoverseasapplicationfrontend.forms
 import play.api.data.Form
 import play.api.data.Forms.{mapping, _}
 import uk.gov.hmrc.agentoverseasapplicationfrontend.models.PersonalDetails
+import uk.gov.voa.play.form.ConditionalMappings.mandatoryIfEqual
 
 object PersonalDetailsForm {
   val form: Form[PersonalDetails] = Form(
     mapping(
       "personalDetailsChoice" -> nonEmptyText,
-      "nino"                  -> optional(text),
-      "saUtr"                 -> optional(text)
+      "nino"                  -> mandatoryIfEqual("personalDetailsChoice", "nino", nonEmptyText),
+      "saUtr"                 -> mandatoryIfEqual("personalDetailsChoice", "saUtr", nonEmptyText)
     )(PersonalDetails.apply)(details =>
       Some((details.choice.value, details.nino.map(_.nino), details.saUtr.map(_.utr))))
   )
