295afbe46fd3d5a71dd7f55c2b2fa3c3c8b4073a
Adam Liversidge <9948188+adamliversidge@users.noreply.github.com>
2019-01-29 09:13:50 +0000

    ASA-655[AL] OA - FE changes from internal DAC review (#55)

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,26 +1,32 @@
-$(function() {
+$(function () {
     //Accessibility
-    var errorSummary =  $('#error-summary-display'),
-    $input = $('input:text')
+    var errorSummary = $('#error-summary-display'),
+        $input = $('input:text');
+
     //Error summary focus
-    if (errorSummary){ errorSummary.focus() }
-    $input.each( function(){
-        if($(this).closest('label').hasClass('form-field--error')){
+    if (errorSummary) {
+        errorSummary.focus()
+    }
+
+    $input.each(function () {
+        if ($(this).closest('label').hasClass('form-field--error')) {
             $(this).attr('aria-invalid', true)
-        }else{
+        } else {
             $(this).attr('aria-invalid', false)
         }
     });
+
     //Trim inputs and Capitalize postode
-    $('[type="submit"]').click(function(){
-        $input.each( function(){
-            if($(this).val() && $(this).attr('data-uppercase') === 'true' ){
+    $('[type="submit"]').click(function () {
+        $input.each(function () {
+            if ($(this).val() && $(this).attr('data-uppercase') === 'true') {
                 $(this).val($(this).val().toUpperCase().replace(/\s\s+/g, ' ').trim())
-            }else{
+            } else {
                 $(this).val($(this).val().trim())
             }
         });
     });
+
     //Add aria-hidden to hidden inputs
     $('[type="hidden"]').attr("aria-hidden", true)
 
@@ -28,98 +34,94 @@ $(function() {
     showHideContent.init()
 
 
-     $('.form-date label.form-field--error').each(function () {
+    $('.form-date label.form-field--error').each(function () {
 
-                $(this).closest('div').addClass('form-field--error')
-                var $relocate = $(this).closest('fieldset').find('legend')
-                $(this).find('.error-notification').appendTo($relocate)
+        $(this).closest('div').addClass('form-field--error')
+        var $relocate = $(this).closest('fieldset').find('legend')
+        $(this).find('.error-notification').appendTo($relocate)
 
+    });
+
+    $('body').on('change', '#country-auto-complete', function () {
+        if (!$(this).val()) {
+            $('#country select option').removeAttr('selected')
+        }
+
+    });
+
+    var selectCountryEl = document.querySelector('#country-auto-complete')
+    if (selectCountryEl) {
+        accessibleAutocomplete.enhanceSelectElement({
+            autoselect: true,
+            defaultValue: selectCountryEl.options[selectCountryEl.options.selectedIndex].innerHTML,
+            minLength: 2,
+            selectElement: selectCountryEl
+        })
+    }
+
+    function findCountry(country) {
+        return country == $("#country-auto-complete").val();
+    }
+
+    //custom handle for not found countries
+    $('#country-auto-complete').change(function () {
+        var changedValue = $(this).val()
+        var array = [];
+
+        $('.autocomplete__menu li').each(function () {
+            array.push($(this).text())
         })
 
+        if (array == "No results found") {
+            $('#country-auto-complete-select').append('<option id="notFound" value="NOTFOUND">No results found</option>')
+            $('#country-auto-complete-select').val('NOTFOUND').attr("selected", "selected");
 
-      var showHideContent = new GOVUK.ShowHideContent()
-      showHideContent.init()
+        } else if (array == "") {
+            $('#country-auto-complete-select').val('').attr("selected", "selected");
+        }
 
-        $('body').on('change', '#country-auto-complete', function(){
-           if(!$(this).val()){
-                       $('#country select option').removeAttr('selected')
-                  }
-
-        });
-
-       var selectCountryEl = document.querySelector('#country-auto-complete')
-          if(selectCountryEl){
-              accessibleAutocomplete.enhanceSelectElement({
-                autoselect: true,
-                defaultValue: selectCountryEl.options[selectCountryEl.options.selectedIndex].innerHTML,
-                minLength: 2,
-                selectElement: selectCountryEl
-              })
-          }
-
-          function findCountry(country) {
-              return  country == $("#country-auto-complete").val();
-          }
-
-        //custom handle for not found countries
-        $('#country-auto-complete').change(function(){
-            var changedValue = $(this).val()
-            var array = [];
-
-            $('.autocomplete__menu li').each(function(){
-                array.push($(this).text())
-            })
-
-            if(array == "No results found"){
-                $('#country-auto-complete-select').append('<option id="notFound" value="NOTFOUND">No results found</option>')
-                $('#country-auto-complete-select').val('NOTFOUND').attr("selected", "selected");
-
-            }else if(array == ""){
-                $('#country-auto-complete-select').val('').attr("selected", "selected");
-            }
-
-        });
+    });
 
 
-       var selectAmlsEl = document.querySelector('#amls-auto-complete')
+    var selectAmlsEl = document.querySelector('#amls-auto-complete')
 
-          if(selectAmlsEl){
-              accessibleAutocomplete.enhanceSelectElement({
-                autoselect: true,
-                defaultValue: selectAmlsEl.options[selectAmlsEl.options.selectedIndex].innerHTML,
-                minLength: 2,
-                selectElement: selectAmlsEl
-              })
-          }
+    if (selectAmlsEl) {
+        accessibleAutocomplete.enhanceSelectElement({
+            autoselect: true,
+            defaultValue: selectAmlsEl.options[selectAmlsEl.options.selectedIndex].innerHTML,
+            minLength: 2,
+            selectElement: selectAmlsEl
+        })
+    }
 
-        //custom handler for AMLS auto-complete dropdown
-        $('#amls-auto-complete').change(function(){
-            var changedValue = $(this).val()
-            var array = [];
+    //custom handler for AMLS auto-complete dropdown
+    $('#amls-auto-complete').change(function () {
+        var changedValue = $(this).val()
+        var array = [];
 
-            $('.autocomplete__menu li').each(function(){
-                array.push($(this).text())
-            })
+        $('.autocomplete__menu li').each(function () {
+            array.push($(this).text())
+        })
 
-            if(array == "No results found"){
-                $('#amls-auto-complete-select').append('<option id="notFound" value="NOTFOUND">No results found</option>')
-                $('#amls-auto-complete-select').val('NOTFOUND').attr("selected", "selected");
+        if (array == "No results found") {
+            $('#amls-auto-complete-select').append('<option id="notFound" value="NOTFOUND">No results found</option>')
+            $('#amls-auto-complete-select').val('NOTFOUND').attr("selected", "selected");
 
-            }else if(array == ""){
-                $('#amls-auto-complete-select').val('').attr("selected", "selected");
-            }
+        } else if (array == "") {
+            $('#amls-auto-complete-select').val('').attr("selected", "selected");
+        }
 
-        });
+    });
 
-         $('.form-date label.form-field--error').each(function () {
+    $('.form-date label.form-field--error').each(function () {
 
-                    $(this).closest('div').addClass('form-field--error')
-                    var $relocate = $(this).closest('fieldset').find('legend')
-                    $(this).find('.error-notification').appendTo($relocate)
+        $(this).closest('div').addClass('form-field--error')
+        var $relocate = $(this).closest('fieldset').find('legend')
+        $(this).find('.error-notification').appendTo($relocate)
 
-            })
+    });
 
-        //by default the amlsForm will be hidden so we we need this to make the form visible after loaded
-        $('#amlsForm').css('visibility', 'visible')
+    //by default the amlsForm will be hidden so we we need this to make the form visible after loaded
+    $('#amlsForm').css('visibility', 'visible')
 
 });
